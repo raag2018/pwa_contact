049fdd7285b34ccbb112d47a03f51f889dbfba66
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-09-08 15:49:50 -0600

    service worket

---
+++ js/app.js
@@ -0,0 +1,5 @@
+if ('serviceWorket' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('sw registered ', reg))
+        .catch(err => console.log('sw register err ', err));
+}
